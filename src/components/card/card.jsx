5dc97ab8b53a695328ec2b1785ecf3234d848283
VNeskoromnaya <neskoromnaya.v@gmail.com>
2022-06-19 11:33:41 +0300

    add useState for Card component

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
+
 import './card.scss';
 
+const TranslationBtn = () => (
+    <button className="btn-translation">Translation</button>
+)
+
+const TranslationWord  = () => (
+    <h2 className="translation">багаж</h2> 
+)
 
 
 const Card = () => {
+
+    const [pressed, setPressed] = useState(false);
+
+    const onPressed = () => {
+        setPressed(!pressed)
+    }
+
     return (
         <div className="card">
             <h1 className="word">luggage</h1>
             <h2 className="transcription">[ ˈlʌɡɪdʒ ]</h2>
-            <div className="translation-wrapper">
-                <button className="btn-translation">Translation</button>
-                <h2 className="translation">багаж</h2>
+            <div className="translation-wrapper" onClick={onPressed}>
+            {!pressed ? <TranslationBtn /> : <TranslationWord />}
             </div>
             <h3 className="topic">Тема: Airport</h3>
             <div className="icons-wrapper">
